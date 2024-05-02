6250c9268eceaff3223d8b10604a1436f2dd5928
Heisen <leonardopjh30@gmail.com>
2024-05-02 09:38:19 -0300

    feat(header): menu styling

---
+++ Assets/JavaScript/Index.js
@@ -0,0 +1,6 @@
+// Var Global
+
+var teste = document.querySelector("");
+var teste1 = document.querySelector("");
+var teste2 = document.querySelector("");
+var teste3 = document.querySelector("");
